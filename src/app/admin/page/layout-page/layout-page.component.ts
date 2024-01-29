559ab4e8a67b6fca3b0f94108586ed36ff2eaa9b
RodrigoCG <rchuquimia94@gmail.com>
2024-01-28 22:14:19 -0400

    sidebar angular material

--- src/app/admin/page/layout-page/layout-page.component.ts
+++ src/app/admin/page/layout-page/layout-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -6,9 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout-page.component.css']
 })
 export class LayoutPageComponent {
+  // sidebarItems = [
+  //   { label: 'Listado', icon: 'label', url: './list' },
+  //   { label: 'Añadir', icon: 'add', url: './new-hero' },
+  //   { label: 'Buscar', icon: 'search', url: './search' },
+  // ];
+
+  // sidebarItems = [
+  //   { icon: 'label', url: '/heroes/index' },
+  //   { icon: 'add', url: './new-hero' },
+  //   { icon: 'search', url: './search' },
+  // ];
+  
   sidebarItems = [
-    { label: 'Listado', icon: 'label', url: './list' },
-    { label: 'Añadir', icon: 'add', url: './new-hero' },
-    { label: 'Buscar', icon: 'search', url: './search' },
+    { icon: 'add', url: './list', tooltip: 'Aplicacion' },
+    { icon: 'info', url: './list', tooltip: 'Aplicacion' },
+    // Otros elementos
   ];
+  
+
+  sidebarWidth = '50px'; // 
+
+  
+  constructor(private router: Router) {} 
+
+  onSidenavItemClicked(item: any): void {
+    if (item.url) {
+      this.router.navigateByUrl(item.url);
+   
+      // this.sidenav.close();
+    }
+  }
 }
